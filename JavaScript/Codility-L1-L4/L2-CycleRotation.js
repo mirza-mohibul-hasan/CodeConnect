803538cc0f030ee2be870ccbc4e357f2be87ea94
Mirza Mohibul Hasan <73751848+mirza-mohibul-hasan@users.noreply.github.com>
2024-01-08 00:39:25 +0600

    Optimal Solutions Added with complexity L2-CycleRotation.js

--- JavaScript/Codility-L1-L4/L2-CycleRotation.js
+++ JavaScript/Codility-L1-L4/L2-CycleRotation.js
@@ -1,3 +1,27 @@
+// Optimal TC: O(N) & MC: O(1)
+function solution(A, K) {
+    const len = A.length;
+    for (let i = 0; i < len / 2; i++) {
+        [A[i], A[len - 1 - i]] = [A[len - 1 - i], A[i]];
+    }
+    K = K % len;
+    for (let i = 0; i < K / 2; i++) {
+        [A[i], A[K - 1 - i]] = [A[K - 1 - i], A[i]];
+    }
+    for (let i = K; i < (len + K) / 2; i++) {
+        [A[i], A[len - 1 + K - i]] = [A[len - 1 + K - i], A[i]];
+    }
+    return A;
+}
+// Optimal TC:O(N) & MC: O(N)
+function solution(A, K) {
+    let ans = []
+    for (let i = 0; i < A.length; i++) {
+        ans[(i + K)%A.length] = A[i];
+    }
+    return ans;
+}
+// Bruteforce TC: O(N^2) & MC: O(1)
 function solution(A, K) {
     if(A.length == 0) return [];
     while (K--) {
